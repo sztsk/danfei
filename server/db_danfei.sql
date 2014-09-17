9c9ee531cba633ba589fba1e4b2db0f47e49ed1c
hugo <baofen14787@gmail.com>
2014-09-17 12:34:12 +0800

    最新

--- server/db_danfei.sql
+++ server/db_danfei.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.3
+-- version 3.5.1
 -- http://www.phpmyadmin.net
 --
 -- 主机: localhost
--- 生成日期: 2014 年 09 月 17 日 10:53
--- 服务器版本: 5.0.26-log
--- PHP 版本: 5.3.6
+-- 生成日期: 2014 年 09 月 17 日 04:33
+-- 服务器版本: 5.5.24-log
+-- PHP 版本: 5.3.13
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -28,10 +28,10 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `tb_company`;
 CREATE TABLE IF NOT EXISTS `tb_company` (
-  `company_id` int(11) NOT NULL auto_increment COMMENT '公司ID',
-  `company_size` varchar(200) character set utf8 default NULL COMMENT '公司规模',
-  `company_intro` text character set utf8 COMMENT '公司简介',
-  PRIMARY KEY  (`company_id`)
+  `company_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '公司ID',
+  `company_size` varchar(200) CHARACTER SET utf8 DEFAULT NULL COMMENT '公司规模',
+  `company_intro` text CHARACTER SET utf8 COMMENT '公司简介',
+  PRIMARY KEY (`company_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -42,7 +42,7 @@ CREATE TABLE IF NOT EXISTS `tb_company` (
 
 DROP TABLE IF EXISTS `tb_events`;
 CREATE TABLE IF NOT EXISTS `tb_events` (
-  `events_id` int(11) NOT NULL auto_increment COMMENT '活动id',
+  `events_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '活动id',
   `events_title` varchar(255) NOT NULL COMMENT '活动标题',
   `events_user_id` int(11) NOT NULL COMMENT '用户ID',
   `events_organizer` int(11) NOT NULL COMMENT '发起人',
@@ -54,7 +54,7 @@ CREATE TABLE IF NOT EXISTS `tb_events` (
   `events_users_num` int(11) NOT NULL COMMENT '报名人数',
   `events_detail` text NOT NULL COMMENT '活动详情',
   `events_zan` int(11) NOT NULL COMMENT '赞的个数',
-  PRIMARY KEY  (`events_id`)
+  PRIMARY KEY (`events_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -65,20 +65,20 @@ CREATE TABLE IF NOT EXISTS `tb_events` (
 
 DROP TABLE IF EXISTS `tb_jobs`;
 CREATE TABLE IF NOT EXISTS `tb_jobs` (
-  `jobs_id` int(11) NOT NULL auto_increment COMMENT '工作ID',
+  `jobs_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '工作ID',
   `jobs_add_time` datetime NOT NULL COMMENT '添加时间',
   `jobs_company_id` int(11) NOT NULL COMMENT '公司ID',
-  `jobs_city` varchar(10) character set utf8 NOT NULL COMMENT '工作城市',
-  `jobs_title` varchar(200) character set utf8 NOT NULL COMMENT '工作职位',
+  `jobs_city` varchar(10) CHARACTER SET utf8 NOT NULL COMMENT '工作城市',
+  `jobs_title` varchar(200) CHARACTER SET utf8 NOT NULL COMMENT '工作职位',
   `jobs_salary_start` tinyint(4) NOT NULL COMMENT '薪酬范围-最低',
   `jobs_salary_end` tinyint(4) NOT NULL COMMENT '薪酬范围-最高',
-  `jobs_intro` text character set utf8 NOT NULL COMMENT '一句话介绍',
+  `jobs_intro` text CHARACTER SET utf8 NOT NULL COMMENT '一句话介绍',
   `jobs_experience` int(11) NOT NULL COMMENT '最低工作经验',
   `jobs_education` int(11) NOT NULL COMMENT '最低学历',
   `jobs_users_num` int(11) NOT NULL COMMENT '该职位的招聘人数',
-  `jobs_detail` text character set utf8 NOT NULL COMMENT '职位详细信息',
+  `jobs_detail` text CHARACTER SET utf8 NOT NULL COMMENT '职位详细信息',
   `jobs_zan` int(11) NOT NULL COMMENT '多少个赞',
-  PRIMARY KEY  (`jobs_id`)
+  PRIMARY KEY (`jobs_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -89,23 +89,23 @@ CREATE TABLE IF NOT EXISTS `tb_jobs` (
 
 DROP TABLE IF EXISTS `tb_users`;
 CREATE TABLE IF NOT EXISTS `tb_users` (
-  `user_id` int(11) NOT NULL auto_increment COMMENT '用户id',
+  `user_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户id',
   `user_name` varchar(200) NOT NULL COMMENT '用户名',
   `user_password` varchar(35) NOT NULL COMMENT '2次md5',
   `user_power` varchar(200) NOT NULL COMMENT '用户权限',
   `user_email` int(11) NOT NULL COMMENT '用户邮箱',
-  `user_phone` varchar(20) default NULL COMMENT '用户手机',
-  `user_qq` varchar(15) default NULL COMMENT 'QQ',
+  `user_phone` varchar(20) DEFAULT NULL COMMENT '用户手机',
+  `user_qq` varchar(15) DEFAULT NULL COMMENT 'QQ',
   `user_jobs_time` int(11) NOT NULL COMMENT '工作年限',
   `user_education` tinyint(4) NOT NULL COMMENT '学历',
   `user_city` varchar(200) NOT NULL COMMENT '城市',
   `user_title` varchar(200) NOT NULL COMMENT '职位',
   `user_salary` varchar(200) NOT NULL COMMENT '期望薪酬',
   `user_intro` text NOT NULL COMMENT '个人简介',
-  `user_time` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP COMMENT '用户加入时间',
+  `user_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '用户加入时间',
   `user_ip` varchar(20) NOT NULL COMMENT '用户注册ip',
-  `user_state` tinyint(4) NOT NULL default '1' COMMENT '用户状态，1是正常，0是无法使用',
-  PRIMARY KEY  (`user_id`),
+  `user_state` tinyint(4) NOT NULL DEFAULT '1' COMMENT '用户状态，1是正常，0是无法使用',
+  PRIMARY KEY (`user_id`),
   UNIQUE KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
@@ -117,13 +117,13 @@ CREATE TABLE IF NOT EXISTS `tb_users` (
 
 DROP TABLE IF EXISTS `tb_zan`;
 CREATE TABLE IF NOT EXISTS `tb_zan` (
-  `zan_id` int(11) NOT NULL auto_increment COMMENT '赞id',
-  `zan_events_id` int(11) default NULL,
-  `zan_users_id` int(11) default NULL,
-  `zan_jobs_id` int(11) default NULL,
-  `zan_company_id` int(11) default NULL,
-  `zan_project_id` int(11) default NULL,
-  PRIMARY KEY  (`zan_id`)
+  `zan_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '赞id',
+  `zan_events_id` int(11) DEFAULT NULL,
+  `zan_users_id` int(11) DEFAULT NULL,
+  `zan_jobs_id` int(11) DEFAULT NULL,
+  `zan_company_id` int(11) DEFAULT NULL,
+  `zan_project_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`zan_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
