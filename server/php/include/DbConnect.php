76f8fb206c5127c78af7241c3c4c3d43ce6b78b0
hugo <baofen14787@gmail.com>
2014-09-17 13:17:09 +0800

    新增注册api  未完成

--- server/php/include/DbConnect.php
+++ server/php/include/DbConnect.php
@@ -3,8 +3,7 @@
 /**
  * Handling database connection
  *
- * @author Ravi Tamada
- * @link URL Tutorial link
+ * @author hugo hua
  */
 class DbConnect {
 
@@ -19,9 +18,10 @@ class DbConnect {
      */
     function connect() {
         include_once dirname(__FILE__) . '/Config.php';
-
+        include_once dirname(dirname(__FILE__)) . '/libs/ezSQL/shared/ez_sql_core.php';
+        include_once dirname(dirname(__FILE__)) . '/libs/ezSQL/pdo/ez_sql_pdo.php';
         // Connecting to mysql database
-        $this->conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+        $this->conn = new ezSQL_pdo("mysql:host=DB_HOST;dbname=DB_NAME;charset=utf8", DB_USERNAME, DB_PASSWORD);
 
         // Check for database connection error
         if (mysqli_connect_errno()) {
